3b46a0207e5e4eafeb0c4910a3dbc4608e233102
schoeu <quanyou1013@gmail.com>
2016-04-15 22:30:50 +0800

    update change sTouch.js

--- src/sTouch-pro.js
+++ src/sTouch-pro.js
@@ -14,29 +14,37 @@ void function (window,sTouch){
     var isIPhone = /iPhone/gi.test(ua);
     var isIpad = /iPad/gi.test(ua);
     var isMobile = isAndroid || isIPhone || isIpad;
-    var hasTouch = 'ontouchstart' in w;
-    var sTouchEvt = 'tap,doubleTap,singleTap,longTap,swipe,swipeLeft,swipeRight,swipeUp,swipeDown';
     var _events = {};
 
-    if(!hasTouch) {
+    if(!isMobile) {
         return;
     }
 
-    function STouch() {
+    function STouch(ele) {
+        var ele = null;
+        if (typeof ele === 'string') {
+            ele = doc.querySelector('ele');
+        }
+        if (!ele instanceof HTMLElement) {
+            throw new Error('wrong arguments');
+        }
+        this.target = ele;
 
+        this._process();
     }
-
+    w.$= STouch;
 
     // 事件&逻辑处理
-    Scroll.prototype = {
-        process: function () {
-            /**
+    STouch.prototype = {
+        _process: function () {
+            var ele = this.target;
+            var me = this;
             var touchStamp = 0, cgStamp=0, oriX= 0, oriY= 0,
                 changeX=0, changeY= 0, isDrag=false, deltaX= 0, deltaY= 0, delta,
                 isSwipe=false;
 
             //touch事件中，根据条件触发不同的自定义事件
-            oriAddEvent.call(selfELe, 'touchstart', function(e){
+            ele.addEventListener('touchstart', function(e){
                 touchStamp = e.timeStamp;
                 isDrag = true;
                 oriX = e.touches[0].clientX;
@@ -45,7 +53,7 @@ void function (window,sTouch){
             }, false);
 
             var crtX = 0,crtY = 0;
-            oriAddEvent.call(selfELe, 'touchmove', function(e){
+            ele.addEventListener('touchmove', function(e){
                 if (isDrag) {
                     crtX = e.touches[0].clientX;
                     crtY = e.touches[0].clientY;
@@ -54,50 +62,48 @@ void function (window,sTouch){
                 }
             });
 
-            oriAddEvent.call(selfELe, 'touchend', function (e){
+            ele.addEventListener('touchend', function (e){
                 cgStamp = e.timeStamp - touchStamp;
                 deltaX = Math.abs(changeX);
                 deltaY = Math.abs(changeY);
                 delta = Math.sqrt(Math.pow(deltaX,2)+Math.pow(deltaY,2));
                 if (type === 'tap' && deltaX < 30 && deltaY < 30){
                     //tap间隔时限符合则触发tap deltaX < 30 && deltaY < 30
-                    evtObj.fireEvent();
+                    me._trigger('tap');
                 }
                 else if (type.indexOf('swipe') > -1) {
                     isSwipe = deltaX > 30 || deltaY > 30;
                     if (type === 'swipe' && isSwipe ){
                         //swipe (Math.abs(touch.x1 - touch.x2) > 30) ||(Math.abs(touch.y1 - touch.y2) > 30)
                         isSwipe = true;
-                        evtObj.fireEvent();
+                        me._trigger('swipe');
                     }
                     if (type === 'swipeLeft' && changeX < -30 && changeY < 100) {
                         //swipeLeft
-                        isSwipe && evtObj.fireEvent();
+                        isSwipe && me._trigger('swipeLeft');
                     }
                     if (type === 'swipeRight' && changeX > 30  && changeY < 100) {
                         //swipeRight
-                        isSwipe && evtObj.fireEvent();
+                        isSwipe && me._trigger('swipeRight');
                     }
                     if (type === 'swipeUp' && changeX < 100 && changeY < -30) {
                         //swipeUp
-                        isSwipe && evtObj.fireEvent();
+                        isSwipe && me._trigger('swipeUp');
                     }
                     if(type === 'swipeDown' && changeX < 100 && changeY > 30 ){
                         //swipeDown
-                        isSwipe && evtObj.fireEvent();
+                        isSwipe && me._trigger('swipeDown');
                     }
                 }
 
                 if (type === 'longTap' && cgStamp > 750 && delta >= 0 && delta <= 250) {
-                    //longTap delta > 0 && delta <= 250
-                    evtObj.fireEvent();
+                    me._trigger('longTap');
                 }
                 //恢复标识变量
                 changeX = changeY = deltaX = deltaY = delta = 0;
                 isDrag = false;
                 isSwipe = false;
             },false);
-            **/
 
         },
 
@@ -124,7 +130,7 @@ void function (window,sTouch){
                 this._events[type].splice(index, 1);
             }
         },
-        _execEvent: function (type) {
+        _trigger: function (type) {
             if ( !this._events[type] ) {
                 return;
             }
@@ -138,4 +144,5 @@ void function (window,sTouch){
             }
         }
     };
+
 });
